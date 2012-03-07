5d79623dae571eaf288c8d46b1286312accd9e9c
Luca Pradovera <luca.pradovera@gmail.com>
2012-03-07 16:20:35 +0100

    First commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source :rubygems
+
+gem "punchblock", :path => "/home/luca/mojolingo/punchblock"
+gem "adhearsion", ">=2.0.0.alpha3"
+
+#
+# Here are some example plugins you might like to use. Simply
+# uncomment them and run `bundle install`.
+#
+
+gem 'adhearsion-asterisk'
+# gem 'adhearsion-rails'
+# gem 'adhearsion-activerecord'
+# gem 'adhearsion-ldap'
+# gem 'adhearsion-xmpp'
+# gem 'adhearsion-drb'
---
+++ config/adhearsion.rb
@@ -0,0 +1,45 @@
+# Centralized way to overwrite any Adhearsion platform or plugin configuration
+# - Execute rake adhearsion:config:desc to get the configuration options
+# - Execute rake adhearsion:config:show to get the configuration values
+#
+# To update a plugin configuration you can write either:
+#
+#    * Option 1
+#        Adhearsion.config.<plugin-name> do |config|
+#          config.<key> = <value>
+#        end
+#
+#    * Option 2
+#        Adhearsion.config do |config|
+#          config.<plugin-name>.<key> = <value>
+#        end
+
+Adhearsion.config do |config|
+
+  config.development do |dev|
+    dev.platform.logging.level = :debug
+  end
+
+  ##
+  # Use with Voxeo PRISM or other Rayo installation
+  #
+  # config.punchblock.username = "" # Your XMPP JID for use with Rayo
+  # config.punchblock.password = "" # Your XMPP password
+
+  ##
+  # Use with Asterisk
+  #
+   config.punchblock.platform = :asterisk # Use Asterisk
+   config.punchblock.username = "adhearsion" # Your AMI username
+   config.punchblock.password = "ahn123" # Your AMI password
+   config.punchblock.host = "127.0.0.1" # Your AMI host
+   config.punchblock.port = 5038 # Your AMI port
+end
+
+Adhearsion.router do
+  route 'general' do
+    answer
+    #play 'tt-monkeys'
+    dial 'SIP/200'
+  end
+end
